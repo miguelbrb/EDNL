b03ad57aed13f9a36007abbb120b6101e9dfb53f
Raúl Díez Sánchez <rauldiez20@gmail.com>
2013-03-19 20:37:52 +0100

    Modificado fichero de pruebas del 4 y 5

--- Practica2/prueba4-5.cpp
+++ Practica2/prueba4-5.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <algorithm>
+#include <cstdlib>
 using namespace std;
-#include "Abin_vec0.h"
+#include "Abin_vec0.h"//Vectorial.Estático.
+//#include "Abin.h"//Enlazado.Dinámico.
 #include "ES_Abin.h"
 
 int main()
 {
-    Abin<char> t;
+    Abin<char> t(10);//Estático.
+    //Abin<char> t;//Dinámico.
     leerAbin(t);
     imprimirAbin(t);
-    int m=t.altura();
+    int m=t.altura(t.raizB());
     cout << "Altura del arbol: "<< m << endl;
     int p=t.profundidad(t.hijoDrchoB(t.hijoIzqdoB(t.raizB())));    
     cout << "Profundidad del nodo "<< ": " << p << endl;
