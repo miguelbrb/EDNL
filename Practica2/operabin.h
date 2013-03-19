a21848a41cb2fc6edbfc9e3312baf48f383776a9
Raúl Díez Sánchez <rauldiez20@gmail.com>
2013-03-19 20:03:14 +0100

    Modificado operabin.h. Añadido método 'profundidad'

--- Practica2/operabin.h
+++ Practica2/operabin.h
@@ -35,40 +35,15 @@ int recorre_altura(typename Abin<T>::nodo n, const Abin<T>& A)
     }
     else return 0;
 }
-/*
-template <typename T>
-int profundidad (const Abin<T>& A, const T& e)
-{
-	return buscaProf(A,A.raizB(),e);
-}
-
-template <typename T>
-int buscaProf(const Abin<T>& A, const typename Abin<T>::nodo n,const T& e)
-{
-    if(n!=Abin<T>::NODO_NULO){
-    if (e != A.elemento(n)){
-       return (1+min(buscaProf(A,A.hijoIzqdoB(n),e),buscaProf(A,A.hijoDrchoB(n),e)));
-    }}
-    else return 0;
-}
-*/
-
 
 //Ejercicio 3
 template <typename T>
 int profundidad (const Abin<T>& A, const typename Abin<T>::nodo n)
 {
-    return ((busca_profundidad(A.raizB(),A,n)));
-}
-
-template <typename T>
-int busca_profundidad(typename Abin<T>::nodo n, const Abin<T>& A, const typename Abin<T>::nodo m)
-{
-    if(n!=Abin<T>::NODO_NULO){
-    if (A.elemento(n) != A.elemento(m)) {
-       return (1+max((busca_profundidad(A.hijoIzqdoB(n),A,m)),(busca_profundidad(A.hijoDrchoB(n),A,m))));
-    }}
-    else return 0;
+    if(n==A.raizB()){return 0;}
+    else{
+    	return (1+profundidad(A,A.padreB(n)));
+	}
 }
 
 #endif
--- Practica2/prueba.cpp
+++ Practica2/prueba.cpp
@@ -14,7 +14,7 @@ int main()
     cout << "Numero de nodos: " << n << endl;
     int m=altura(t);
     cout << "Altura del arbol: "<< m << endl;
-    int p=profundidad(t,t.hijoIzqdoB(t.raizB()));    
+    int p=profundidad(t,t.hijoDrchoB(t.hijoIzqdoB(t.raizB())));    
     cout << "Profundidad del nodo "<< ": " << p << endl;
     return 0;
 }
