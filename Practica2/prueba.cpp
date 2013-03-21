995ba5d73828f13681f7cf13c18e79cec346b503
Raúl Díez Sánchez <rauldiez20@gmail.com>
2013-03-21 12:02:04 +0100

    Pruebas con pseudocompleto

--- Practica2/prueba.cpp
+++ Practica2/prueba.cpp
@@ -14,8 +14,8 @@ int main()
     cout << "Numero de nodos: " << n << endl;
     int h=altura(t);
     cout << "Altura del arbol: "<< h << endl;
-    int p=profundidad(t,t.hijoDrchoB(t.hijoIzqdoB(t.raizB())));    
-    cout << "Profundidad del nodo: " << p << endl;
+    //int p=profundidad(t,t.hijoDrchoB(t.hijoIzqdoB(t.raizB())));    
+    //cout << "Profundidad del nodo: " << p << endl;
     int d=desequilibrio(t);
     cout << "Desequilibrio del arbol:" << d << endl;
     if(pseudocompleto(t)){ cout << "Arbol pseudocompleto." << endl; }
