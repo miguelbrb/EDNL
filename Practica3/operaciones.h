cc728fcaf7bd619ba6f87468df6993125c509ed1
Raúl Díez Sánchez <rauldiez20@gmail.com>
2013-04-02 14:36:19 +0200

    Terminado ejercicio 2

--- Practica3/operaciones.h
+++ Practica3/operaciones.h
@@ -21,7 +21,7 @@ bool SimilaresRec(const Abin<T>& A, const typename Abin<T>::nodo a,const Abin<T>
 	{
 		return SimilaresRec(A,A.hijoIzqdoB(a),B,B.hijoIzqdoB(b));
 	}
-	if(A.hijoDrchoB(a)!=Abin<T>::NODO_NULO && B.hijoDrchoBb()!=Abin<T>::NODO_NULO)
+	if(A.hijoDrchoB(a)!=Abin<T>::NODO_NULO && B.hijoDrchoB()!=Abin<T>::NODO_NULO)
 	{
 		return SimilaresRec(A,A.hijoDrchoB(a),B,B.hijoDrchoB(b));
 	}
@@ -50,6 +50,39 @@ bool AbinSimilares (const Abin<T>& A, const typename Abin<T>::nodo a,const Abin<
 	}
 }
 */
-//Ejercicio 2.
+//Ejercicio 2. Reflejar un arbol en otro.
+template <typename T>
+Abin<T> reflejar(const Abin<T>& A)
+{
+	Abin<T> reflejo;
+	if(A.arbolVacioB()){ return reflejo; }
+	else{ 
+		B.crearRaizB(A.elemento(A.raizB()));
+		refleRec(A,A.raizB(),B,B.raizB());
+		return reflejo;
+		}
+}
 
-template <typename T
+template <typename T>
+void refleRec(const Abin<T>& A, const typename Abin<T>::nodo a, Abin<T>& B, const typename Abin<T>::nodo b)
+{
+	if(A.hijoIzqdoB(a)!=Abin<T>::NODO_NULO || A.hijoDrchoB(a)!=Abin<T>::NODO_NULO){//Si tiene algún hijo...
+		
+		if(A.hijoIzqdoB(a)!=Abin<T>::NODO_NULO)
+		{
+			B.insertarHijoDrchoB(b,A.elemento(A.hijoIzqdoB(a)));
+			refleRec(A,A.hijoIzqdoB(a),B,B.hijoDrchoB(b));
+		}
+		if(A.hijoDrchoB(a)!=Abin<T>::NODO_NULO)
+		{
+			B.insertarHijoIzqdoB(b,A.elemento(A.hijoDrchoB(a)));
+			refleRec(A,A.hijoDrchoB(a),B,B.hijoIzqdoB(b));
+		}
+	}
+	else{/*No se hace nada.*/}//No tiene hijos, por tanto no se sigue por esa rama.
+}
+
+
+
+
+
