e59f12493bc7184aa4b6e332532726b4fe152ceb
Raúl Díez Sánchez <rauldiez20@gmail.com>
2013-04-02 14:00:28 +0200

    Terminado ejercicio 1

--- Practica3/operaciones.h
+++ Practica3/operaciones.h
@@ -17,13 +17,35 @@ template <typename T>
 bool SimilaresRec(const Abin<T>& A, const typename Abin<T>::nodo a,const Abin<T>& B, const typename Abin<T>::nodo b)
 {
 	if(a==Abin<T>::NODO_NULO && b==Abin<T>::NODO_NULO) { return true; }
-	if(a.hijoIzqdoB()!=Abin<T>::NODO_NULO && b.hijoIzqdoB()!=Abin<T>::NODO_NULO)
+	if(A.hijoIzqdoB(a)!=Abin<T>::NODO_NULO && B.hijoIzqdoB(b)!=Abin<T>::NODO_NULO)
 	{
-		return return SimilaresRec(A,a.hijoIzqdoB(),B,b.hijoIzqdoB());
+		return SimilaresRec(A,A.hijoIzqdoB(a),B,B.hijoIzqdoB(b));
 	}
-	if(a.hijoDrchoB()!=Abin<T>::NODO_NULO && b.hijoDrchoB()!=Abin<T>::NODO_NULO)
+	if(A.hijoDrchoB(a)!=Abin<T>::NODO_NULO && B.hijoDrchoBb()!=Abin<T>::NODO_NULO)
 	{
-		return return SimilaresRec(A,a.hijoDrchoB(),B,b.hijoDrchoB());
+		return SimilaresRec(A,A.hijoDrchoB(a),B,B.hijoDrchoB(b));
 	}
 	else return false;	
+}
+
+/*
+//OTRA FORMA. No tan eficiente, pero es buena.
+template <typename T>
+bool AbinSimilares (const Abin<T>& A, const typename Abin<T>::nodo a,const Abin<T>& B, const typename Abin<T>::nodo b)
+{
+	if(A.arbolVacioB() && B.arbolVacioB()){ return true; }
+	else{
+		if(a!=Abin<T>::NODO_NULO && b!=Abin<T>::NODO_NULO)
+		{
+			bool ri,rd;
+			
+			ri=AbinSimilares(A,A.hijoIzqdoB(a),B,B.hijoIzqdoB(b));
+			rd=AbinSimilares(A,A.hijoDrchoB(a),B,B.hijoDrchoB(b));
+			
+			return ri && rd;
+		}
+		else if(a==Abin<T>::NODO_NULO && b==Abin<T>::NODO_NULO)
+				{ return true; }
+			else return false;
+	}
 }
