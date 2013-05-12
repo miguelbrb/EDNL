092edad566d996ab201a2c971616caaa30c28dae
Raúl Díez Sánchez <rauldiez20@gmail.com>
2013-05-12 21:11:15 +0200

    Corregido grado, y añadido profundidad.  desequilibrio está MAL.

--- Practica4/operaciones.h
+++ Practica4/operaciones.h
@@ -3,25 +3,50 @@
 #include <algorithm>
 
 template <typename T>
-int gradoAgen(const Agen<T> a)
+int gradoAgen(const Agen<T>& a)
 {
 	return calculaGrado(a, a.raiz());
 }
 
 template <typename T>
-int calculaGrado(const Agen<T> a, typename Agen<T>::nodo n)
+int calculaGrado(const Agen<T>& a, typename const Agen<T>::nodo& n)
 {
-	if(n==Agen<T>::NODO_NULO){ return 0; }
 	int grado = 0;
-	if(a.hijoIzqdo(n)!=Agen<T>::NODO_NULO){//Tiene al menos un hijo:
-		grado=1;
-		grado=max(grado,calculaGrado(a,a.hijoIzqdo(n)));//Se calcula grado del hijoIzqdo.
-		typename Agen<T>::nodo herm=a.hijoIzqdo(n);//herm es hijoIzqdo(n)
-		while(herm.hermDrcho(n)!=Agen<T>::NODO_NULO)//Mientras herm tenga hermanos:
+	if(n==Agen<T>::NODO_NULO){ return 0; }
+	else{
+		typename Agen<T>::nodo aux=n;//aux es copia de n.
+		while(a.hermDrcho(aux)!=Agen<T>::NODO_NULO)//Mientras aux tenga hermanos:
 		{
-			grado=max(grado,calculaGrado(a,a.hermDrcho(herm)));
-			herm=a.hermDrcho(herm);//Se pasa a su hermano.
+			++grado;//Se incrementa el grado según tenga hermanos.
+			aux=a.hermDrcho(aux);//Se pasa a su hermano.
 		}
 	}
-	return grado;//El grado resultante es el máximo del grado de los hijos de n.
+	return max(calculaGrado(a, a.hijoIzqdo(n)), max(calculaGrado(a, a.hermDrcho(n)), ++grado));
+	//Si resultase que n no tiene hermanos, su grado sería 1, por el "++grado" de la llamada recursiva.
+	//El grado resultante es el máximo del grado de los hijos de n, y de los hijos de sus hermanos.
+}
+
+template <typename T>
+int profundidadAgen(const Agen<T>& a, typename const Agen<T>::nodo n)
+{
+	if(n!=a.raiz()){ return 0; }
+	else{
+		return 1+profundidadAgenRec(a, a.padre(n));
+	}
+}
+
+template <typename T>
+int desequilibrioAgen(const Agen<T>& A)
+{
+	return desequilibrioMAX(A,A.raizB());
+}
+template <typename T>
+int desequilibrioMAX(const Agen<T>& A, const typename Agen<T>::nodo n)
+{
+    if(n==Abin<T>::NODO_NULO){return 0;}
+    else
+    {
+        int deseq_nodo=abs(A.altura(A.hijoIzqdoB(n)) - A.altura(A.hijoDrchoB(n)));
+        return max(deseq_nodo,max(desequilibrioMAX(A,A.hijoIzqdoB(n)),desequilibrioMAX(A,A.hijoDrchoB(n))));
+    }
 }
