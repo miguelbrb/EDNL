fccbd36ef02fc57a631840cce482e1a7ff052f08
Raúl Díez Sánchez <rauldiez20@gmail.com>
2013-05-14 12:56:05 +0200

    Añadido desequilibrio, junto con las funciones de altura. Agen.

--- Practica4/operaciones.h
+++ Practica4/operaciones.h
@@ -35,3 +35,48 @@ int profundidadAgen(const Agen<T>& a, const typename Agen<T>::nodo n)
 	}
 }
 
+//Ejercicio 4. Desequilibrio de un Agen. 
+//Son necesarias aquí 2 funciones: alturaAgenMax y alturaAgenMin, que devuelven respectivamente la altura
+//maxima y minima de un Agen.
+
+template <typename T>
+int alturaAgenMax(const Agen<T>& a, const typename Agen<T>::nodo  n)
+{
+	if(n==Agen<T>::NODO_NULO) { return -1; }
+	else{
+		int altura = alturaAgenMax(a,a.hijoIzqdo(n));//Se calcula la altura del hijoIzqdo, que se usa como mínimo.
+		typename Agen<T>::nodo her=a.hermDrcho(n);
+		where(herm!=Agen<T>::NODO_NULO)
+		{
+			altura=max(altura, alturaAgenMax(a,her);//Se va calculando la altura de los hermanos y el máximo entre
+													//estas alturas.
+			her=a.hermDrcho(her);
+		}
+		return 1+altura;
+	}
+}
+
+template <typename T>
+int alturaAgenMin(const Agen<T>& a, const typename Agen<T>::nodo  n)
+{
+	if(n==Agen<T>::NODO_NULO) { return -1; }
+	else{
+		int altura = alturaAgenMax(a,a.hijoIzqdo(n));//Se calcula la altura del hijoIzqdo, que se usa como máximo.
+		typename Agen<T>::nodo her=a.hermDrcho(n);
+		where(herm!=Agen<T>::NODO_NULO)
+		{
+			altura=min(altura, alturaAgenMax(a,her);//La altura que se calcula es la máxima de cada nodo, para luego
+													//calcular el mínimo de estas alturas máximas.
+			her=a.hermDrcho(her);
+		}
+		return 1+altura;
+	}
+}
+
+template <typename T>
+int desequilibrioAgen(const Agen<T>& a)
+{
+	return abs(alturaAgenMax(a, a.raiz()) - alturaAgenMin(a,a.raiz()));
+}
+
+
