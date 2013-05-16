39fe4bd901f316fc8584b1d03f2def6e45fec889
Raúl Díez Sánchez <rauldiez20@gmail.com>
2013-05-16 12:08:23 +0200

    Primeros archivos Examen2-2012.

---
+++ Practica5/Examen2-2012/Abb.h
@@ -0,0 +1,173 @@
+#ifndef ABB_H
+#define ABB_H
+#include <cassert>
+#include <Abin.h>
+
+template <typename T> class Abb {
+	struct celda;   // declaración adelantada privada
+	public:
+		typedef celda* nodo;
+		static const nodo NODO_NULO;
+		explicit Abb(nodo n = NODO_NULO);      // constructor
+		nodo buscar(const T& e) const;
+		void insertar(const T& e);
+		void eliminar(const T& e);
+		bool vacio() const;
+		Abb(const Abb<T>& a);                 // ctor. de copia
+		Abb<T>& operator =(const Abb<T>& a);  // asig. árboles
+		~Abb(); // destructor
+	private:    
+		struct celda {
+			T elto;
+			nodo hizq, hder;
+			celda(const T& e): elto(e),
+			hizq(NODO_NULO), hder(NODO_NULO) {}
+		};
+		nodo r;   // nodo raíz del árbol
+		T borrarMin(nodo& n);
+		nodo copiar(nodo n);
+};
+/* Definición del nodo nulo */
+template <typename T>
+const typename Abb<T>::nodo Abb<T>::NODO_NULO(0);
+template <typename T>
+inline Abb<T>::Abb(nodo n) : r(n) {}
+template <typename T>
+inline bool Abb<T>::vacio() const
+{
+return (r == NODO_NULO);
+}
+template <typename T>
+typename Abb<T>::nodo Abb<T>::buscar(const T& e) const
+{
+nodo n;
+if (r == NODO_NULO)     // árbol vacío, e no encontrado
+n = NODO_NULO;
+else if (e == r->elto)  // encontrado e en la raíz
+n = r;
+else if (e < r->elto) { // buscar en subárbol izqdo.
+Abb<T> Ai(r->hizq);
+n = Ai.buscar(e);
+Ai.r = NODO_NULO;    // impide destruir el subárbol izqdo.
+}
+else {                  // buscar en subárbol drcho.
+Abb<T> Ad(r->hder);
+n = Ad.buscar(e);
+Ad.r = NODO_NULO;    // impide destruir el subárbol drcho.
+}
+return n;
+}
+template <typename T>
+void Abb<T>::insertar(const T& e)
+{
+if (r == NODO_NULO)         // árbol vacío
+r = new celda(e);
+else if (!(e == r->elto)) { // e no está en la raíz.
+if (e < r->elto) {       // insertar en el subárbol izqdo.
+Abb<T> Ai(r->hizq);
+Ai.insertar(e);
+r->hizq = Ai.r;
+Ai.r = NODO_NULO;     // impide destruir el subárbol drcho.
+}
+else {  // insertar en el subárbol drcho.
+Abb<T> Ad(r->hder);
+Ad.insertar(e);
+r->hder = Ad.r;
+Ad.r = NODO_NULO;     // impide destruir el subárbol drcho.
+}
+}
+}
+template <typename T>
+void Abb<T>::eliminar(const T& e)
+{
+if (r != NODO_NULO) {  // árbol no vacío
+if (e == r->elto) { // quitar elemento de la raíz
+if (r->hizq == NODO_NULO && r->hder == NODO_NULO) {//1.Raíz es hoja
+delete(r);
+r = NODO_NULO;   // el árbol queda vacío
+}
+else if (r->hder == NODO_NULO) { // 2.Raíz sólo tiene hijo izqdo.
+nodo n = r->hizq;
+delete(r);
+r = n;                    // nueva raíz el antiguo hijo izqdo.
+}
+else if (r->hizq == NODO_NULO) { // 3.Raíz sólo tiene hijo drcho.
+nodo n = r->hder;
+delete(r);
+r = n;                    // nueva raíz el antiguo hijo drcho.
+}
+else // 4.La raíz tiene dos hijos.
+r->elto = borrarMin(r->hder); // sustituir el elemento de la 
+// raíz por el mínimo del 
+// subárbol derecho
+}
+else if (e < r->elto) {//quitar elemento del subárbol izqdo.
+Abb<T> Ai(r->hizq);
+Ai.eliminar(e);
+r->hizq = Ai.r;
+Ai.r = NODO_NULO; // impide destruir el subárbol izqdo.
+}
+else {               // quitar elemento del subárbol drcho.
+Abb<T> Ad(r->hder);
+Ad.eliminar(e);
+r->hder = Ad.r;
+Ad.r = NODO_NULO; // impide destruir el subárbol drcho.
+}
+}
+}
+// Método privado
+template <typename T>
+T Abb<T>::borrarMin(Abb<T>::nodo& n)
+// Elimina el nodo que almacena el menor elemento
+// del subárbol cuya raíz es n. Devuelve el elemento
+// del nodo eliminado
+{
+if (n->hizq == NODO_NULO) {
+T e = n->elto;
+nodo m = n->hder;
+delete(n);
+n = m;
+return e;
+}
+else
+return borrarMin(n->hizq);
+}
+template <typename T>
+inline Abb<T>::Abb(const Abb<T>& a)
+{
+r = copiar(a.r);
+}
+template <typename T>
+Abb<T>& Abb<T>::operator =(const Abb<T>& a)
+{
+if (this != &a) {   // evitar autoasignación
+this->~Abb();    // vaciar el árbol
+r = copiar(a.r);
+}
+return *this;
+}
+template <typename T>
+inline Abb<T>::~Abb()
+{
+if (r != NODO_NULO) {   // árbol no vacío
+Abb<T> Ai(r->hizq),
+Ad(r->hder);
+delete r;
+}
+}
+// Método privado
+template <typename T>
+typename Abb<T>::nodo Abb<T>::copiar(Abb<T>::nodo n)
+// Devuelve una copia del nodo n y todos sus descendientes
+{
+nodo m = NODO_NULO;
+if (n != NODO_NULO) {
+m = new celda(n->elto);    // copiar n
+m->hizq = copiar(n->hizq); // copiar subárbol izqdo.
+m->hder = copiar(n->hder); // copiar subárbol drcho.
+}
+return m;
+}
+
+
+#endif // ABB_H
---
+++ Practica5/Examen2-2012/ES_Abin.h
@@ -0,0 +1,70 @@
+#ifndef ES_ABIN_H
+#define ES_ABIN_H
+#include <iostream>
+//#include "Abin_vec0.h"//Estático.
+#include "Abin.h"//Dinámico.
+#include "expresion.h"
+using namespace std;
+
+template <typename T>
+void imprimirAbin(const Abin<T>& A)
+{
+    if (!A.arbolVacioB()) {
+        cout << "Raiz del arbol: "
+        << A.elemento(A.raizB()) << endl;
+        imprimirDescendientes(A.raizB(), A);
+    }
+    else
+    cout << "Arbol vacio\n";
+}
+
+template <typename T>
+void imprimirDescendientes (typename Abin<T>::nodo r,const Abin<T>& A)
+{
+     if (A.hijoIzqdoB(r) != Abin<T>::NODO_NULO) {
+        cout << "Hijo izqdo de " << A.elemento(r) << ": "
+        << A.elemento(A.hijoIzqdoB(r)) << endl;
+        imprimirDescendientes(A.hijoIzqdoB(r), A);
+     }
+     if (A.hijoDrchoB(r) != Abin<T>::NODO_NULO) {
+        cout << "Hijo derecho de " << A.elemento(r) << ": "
+        << A.elemento(A.hijoDrchoB(r)) << endl;
+        imprimirDescendientes(A.hijoDrchoB(r), A);
+     }
+}
+
+template <typename T> 
+void leerAbin(Abin<T>& a){
+     if(a.arbolVacioB()){
+		 T elto, bandera;
+		 cout << "Escriba elemento 'bandera' para parar" << endl;
+		 cin >> bandera;
+		 cout << "Introduzca nodo raiz:" << endl;
+		 cin >> elto;
+		 if(elto!=bandera){
+		                   a.crearRaizB(elto);
+		                   leerDescendientes(a, a.raizB(), bandera);
+		                   }
+		 else{
+		      cout << "Fin del arbol." << endl;
+		      }
+		 }else{cout<<"Error: El arbol no estaba vacio."<<endl;}
+     }
+
+template <typename T> 
+void leerDescendientes (Abin<T>& A, typename Abin<T>::nodo n, T bandera){
+     T elto;
+     cout << "Inserta hijo izquierdo de " << A.elemento(n) <<": ('" << bandera << "' no introducir mas hijos por aqui) " << endl;
+     cin >> elto;
+     if(elto != bandera){
+          A.insertarHijoIzqdoB(n,elto);
+          leerDescendientes(A, A.hijoIzqdoB(n), bandera);
+     }
+     cout << "Inserta hijo derecho de " << A.elemento(n) <<": ('" << bandera << "' no introducir mas hijos por aqui) " << endl;
+     cin >> elto;
+             if(elto != bandera){
+                     A.insertarHijoDrchoB(n,elto);
+                     leerDescendientes(A, A.hijoDrchoB(n), bandera);
+             }
+}
+#endif  // ES_ABIN_H
